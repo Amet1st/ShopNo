b33cb898bf6f6222f61b962a321c193dc24c2560
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-07-12 09:30:31 +0300

    fix: layout fixes

--- js/popup.js
+++ js/popup.js
@@ -2,17 +2,17 @@ let modal = document.querySelector(".modal");
 
 let images = document.querySelectorAll(".open-modal");
 
-let modalImg = document.querySelector(".modal-content");
+let modalImg = document.querySelector(".modal__content");
 
 
 images.forEach(item => {
     item.addEventListener('click', (event) => {
-        modal.style.display = "block";
+        modal.style.display = "flex";
         modalImg.src = event.target.src;
     })
 })
 
-let span = document.querySelector(".close");
+let span = document.querySelector(".modal__close");
 
 span.onclick = function () {
     modal.style.display = "none";
